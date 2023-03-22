7a6f8e1ee6371aada215bb8d21ead2472c28d8cf
MELISSA AUGUSTO RIBEIRO <melissa.ribeiro@afya.com.br>
2023-03-21 21:21:05 -0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Melissa Ribeiro on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
